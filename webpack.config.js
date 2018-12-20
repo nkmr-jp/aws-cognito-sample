191a1c9bb6caa3757ec33115a3a71f1e226af8aa
nkmr-jp <nkmr.git@gmail.com>
2018-12-20 23:57:20 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mode: 'development',
+  // Example setup for your project:
+  // The entry module that requires or imports the rest of your project.
+  // Must start with `./`!
+  entry: './src/entry.js',
+  // html: './index.html',
+  // Place output files in `./dist/my-app.js`
+  output: {
+    path: __dirname + '/dist',
+    filename: 'my-app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  }
+};
