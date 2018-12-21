a771aaecc6b9841056b27f9df8c548c09e7dd403
nkmr-jp <nkmr.git@gmail.com>
2018-12-21 18:55:17 +0900

    add readme, form, signup.

---
+++ README.md
@@ -0,0 +1,22 @@
+## Install and Build
+
+```
+$ yarn
+$ yarn run build
+```
+
+## Server
+```
+$ npm install -g superstatic
+$ superstatic -p 9001
+Superstatic started.
+Visit http://localhost:9001 to view your app.
+```
+
+## LiveReload
+```
+$ npm install -g livereload
+$ livereload . -w 500 -d
+```
+
+- Install chrome extension [LiveReload](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei?utm_source=chrome-ntp-icon)
--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,57 @@
-console.log('test')
+// Modules, e.g. Webpack:
+
+// console.log(process.env.NODE_ENV);
+window.console.log('hoge3');
+
+function signUp(name,mail,phone,password) {
+    var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+    var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
+
+    var poolData = {
+        UserPoolId: '', // Your user pool id here
+        ClientId: '' // Your client id here
+    };
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+    var attributeList = [];
+    var dataEmail = {
+        Name: 'email',
+        Value: mail
+    };
+    var dataPhoneNumber = {
+        Name: 'phone_number',
+        Value: phone
+    };
+
+    var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+    var attributePhoneNumber = new AmazonCognitoIdentity.CognitoUserAttribute(dataPhoneNumber);
+
+    attributeList.push(attributeEmail);
+    attributeList.push(attributePhoneNumber);
+
+    userPool.signUp(
+        name,
+        password,
+        attributeList,
+        null, function (err, result) {
+            if (err) {
+                alert(err.message || JSON.stringify(err));
+                return;
+            }
+            var cognitoUser = result.user;
+            console.log('user name is ' + cognitoUser.getUsername());
+        });
+}
+
+const myForm = document.querySelector('.signup');
+myForm.addEventListener('submit', handleForm, false);
+
+function handleForm(event){
+    event.preventDefault();
+    var name = document.getElementById('InputName').value;
+    var mail = document.getElementById('InputEmail').value;
+    var phone = document.getElementById('InputPhone').value;
+    var password = document.getElementById('InputPassword').value;
+    signUp(name,mail,phone,password);
+    myForm.submit();
+}
+
