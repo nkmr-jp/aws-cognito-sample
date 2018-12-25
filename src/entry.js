4f0734be077bc8b14ff85e51ac1603b5cee33cfe
nkmr-jp <nkmr.git@gmail.com>
2018-12-25 12:19:55 +0900

    :art::bug: fix bug and Refactoring

--- src/entry.js
+++ src/entry.js
@@ -1,11 +1,20 @@
 var env = require('./env');
 var ACI = require('amazon-cognito-identity-js');
-var getById = document.getElementById;
+
 
 var userPool = new ACI.CognitoUserPool({
     UserPoolId: env.AWS_COGNITO_USER_POOL_ID,
     ClientId: env.AWS_COGNITO_CLIENT_ID
 });
+
+function cognitoCallBack(err, result) {
+    if (err) {
+        alert(err.message || JSON.stringify(err));
+        return;
+    }
+    var cognitoUser = result.user;
+    console.log('user name is ' + cognitoUser.getUsername());
+}
 
 function signUp(name, mail, phone, password) {
     var attributeList = [
@@ -14,26 +23,24 @@ function signUp(name, mail, phone, password) {
     ];
 
     userPool.signUp(name, password, attributeList, null,
-        function (err, result) {
-            if (err) {
-                alert(err.message || JSON.stringify(err));
-                return;
-            }
-            var cognitoUser = result.user;
-            console.log('user name is ' + cognitoUser.getUsername());
-        }
+        cognitoCallBack
     );
 }
 
-const signUpForm = document.querySelector('.signup');
-signUpForm.addEventListener('submit', handleSignUp, false);
-
 function handleSignUp(event) {
     event.preventDefault();
-    var name = getById('InputName').value;
-    var mail = getById('InputEmail').value;
-    var phone = getById('InputPhone').value;
-    var password = getById('InputPassword').value;
-    signUp(name, mail, phone, password);
-    signUpForm.submit();
+    signUp(
+        document.getElementById('InputName').value,
+        document.getElementById('InputEmail').value,
+        document.getElementById('InputPhone').value,
+        document.getElementById('InputPassword').value
+    );
+    // signUpForm.submit();
 }
+
+function initSignUp() {
+    const signUpForm = document.getElementById('FormSingUp');
+    signUpForm.addEventListener('submit', handleSignUp, false);
+}
+
+initSignUp();
